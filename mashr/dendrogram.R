26fc1cc7fa3593271df10dde3cfd5cb1947fbe7f
Mollie Peters <mjpete11@asu.edu>
2020-11-30 12:15:52 -0700

    dendrograms of sex effects

--- mashr/dendrogram.R
+++ mashr/dendrogram.R
@@ -1,133 +1,78 @@
 # Make dendrograms based on total gene expression and sex biased genes
 
 # libraries
-library(tidyverse)
 library(mashr)
-library(stringr)
+library(data.table)
+library(dendextend)
 
 # Constants
 BASE <- "/scratch/mjpete11/human_monkey_brain/mashr/"
 
 # Input
-mash_results = readRDS('/scratch/mjpete11/human_monkey_brain/mashr/output/mashr_results.rds')
-mash_beta = get_pm(mash_results)
-mash_sbet = get_pm(mash_results) / get_psd(mash_results)
-mash_lfsr = get_lfsr(mash_results)
+female <- readRDS(paste0(BASE, "female_gene_counts/female_lst.rds"))
+male <- readRDS(paste0(BASE, "male_gene_counts/male_lst.rds"))
+mash_results = readRDS(paste0(BASE, "output/mashr_results.rds"))
 
 # Output
-#ALL_EUCLIDEAN_COMPLETE <- paste0(BASE, "dendrograms/", "all_euclidean_complete.pdf")
+ALL_GENES <- paste0(BASE, "dendrograms/all_genes.pdf")
+MALE <- paste0(BASE, "dendrograms/male.pdf")
+FEMALE <- paste0(BASE, "dendrograms/female.pdf")
 
 #_______________________________________________________________________________
-# Read in an organize sex-biased gene effect sizes in each region  
+# tissue phylogeny based on female- ande male-baised genes only  
 #_______________________________________________________________________________
-regions <- colnames(mash_beta)
+# Prep region names
+regions <- c("amygdala", "anterior_cortex", "caudate", "cerebellum",
+			 "frontal_cortex", "hippocampus", "hypothalamus", 
+			 "nucleus_accumbens", "putamen", "spinal_cord", "substantia_nigra")
 
-# Read in male results as list of dfs
-setwd(paste0(BASE, "male_gene_counts/"))
-mfiles <- list.files()
-mlst <- lapply(mfiles, read.csv)
+# female sDEG matrix
+fem_df <- t(rbindlist(female, fill = TRUE))
+colnames(fem_df) <- regions
 
-# Read in female " " " "
-setwd(paste0(BASE, "female_gene_counts/"))
-ffiles <- list.files()
-flst <- lapply(ffiles, read.csv)
+# male sDEG matrix
+male_df <- t(rbindlist(male, fill = TRUE))
+colnames(male_df) <- regions
 
-# Drop rownumber column and drop "." in column names
-drop_col <- function(x) {
-		x <- x[, -c(1)]
-		colnames(x) <- str_replace_all(colnames(x), pattern = "\\.", "") 
-		return(x)
+# Replace missing values with zero (for now)
+fem_df[is.na(fem_df)] <- 0
+male_df[is.na(male_df)] <- 0
+
+# Similarity matrix
+fem_dist <- dist(t(fem_df), method = "euclidean") 
+male_dist <- dist(t(male_df), method = "euclidean") 
+
+# Complete linkage clustering
+fres <- hclust(fem_dist, method = "complete")
+mres <- hclust(male_dist, method = "complete")
+
+# Plot function
+plot_dendrogram <- function(output, object, head_title) {
+		pdf(output)
+		par(cex = 0.8)
+		plot(object, main = NULL, ylab = NULL, xlab = NULL)
+		par(cex = 1)
+		title(main = paste(head_title,  
+	    "Euclidean distance with complete linkage clustering", sep = "\n"), 
+	  	xlab = NULL, ylab = "Height")
+		dev.off()
 }
-mlst <- lapply(mlst, drop_col)
-flst <- lapply(flst, drop_col)
+plot_dendrogram(output = FEMALE, object = fres, head_title = c("Dendrogram of significant female effect sizes;"))
+plot_dendrogram(output = MALE, object = mres, head_title = c("Dendrogram of significant male effect sizes;"))
 
 #_______________________________________________________________________________
-# Experiment with different similarty measures and clustering methods 
+# dendrogram of all gene effects 
 #_______________________________________________________________________________
+# Prepare df
+gene_df <- get_pm(mash_results)
+colnames(gene_df) <- regions
+
 # Euclidean distance with complete, avergae, and single linkage clustering  
 # Calculate the Euclidean distance across genes within the same region
-euclidean <- dist(t(mash_beta), method = "euclidean") 
+genes <- dist(t(gene_df), method = "euclidean") 
 
 # Complete linkage clustering
-res1 <- hclust(euclidean, method = "complete")
+res1 <- hclust(genes, method = "complete")
 
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "all_euclidean_complete.pdf"))
-plot(res1, main = "Euclidean distance with complete linkage clustering")
-par(crt = 0.7)
-dev.off()
-
-# Euclidean ditance with average linkage clustering
-res2 <- hclust(euclidean, method = "average")
-
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "all_euclidean_average.pdf"))
-plot(res2, main = "Euclidean distance with average linkage clustering")
-dev.off()
-
-# Euclidean distance with single linkage clustering
-res3 <- hclust(euclidean, method = "single")
-
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "all_euclidean_single.pdf"))
-plot(res3, main = "Euclidean distance with average linkage clustering")
-dev.off()
-
-#_______________________________________________________________________________
-# Manhattan distance with complete, avergae, and single linkage clustering 
-#_______________________________________________________________________________
-# Calculate the Euclidean distance between gene effect sizes
-manhattan <- dist(t(mash_beta), method = "manhattan") 
-
-# Complete linkage clustering
-res4 <- hclust(manhattan, method = "complete")
-
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "all_manhattan_complete.pdf"))
-plot(res4, main = "Manhattan distance with complete linkage clustering")
-dev.off()
-
-# Manhattan distance with average linkage clusterin
-res5 <- hclust(manhattan, method = "average")
-
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "all_manhattan_average.pdf"))
-plot(res5, main = "Manhattan distance with average linkage clustering")
-dev.off()
-
-# Manhattan distance with single linkage clustering
-res6 <- hclust(manhattan, method = "single")
-
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "all_manhattan_single.pdf"))
-plot(res6, main = "Manhattan distance with single linkage clustering")
-dev.off()
-
-#_______________________________________________________________________________
-# Repeat with standardized betas 
-#_______________________________________________________________________________
-euclidean <- dist(t(mash_sbet), method = "euclidean") 
-
-# Complete linkage clustering
-res1 <- hclust(euclidean, method = "complete")
-
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "sbet_euclidean_complete.pdf"))
-plot(res1, main = "Euclidean distance of standardized betas with complete linkage clustering")
-dev.off()
-
-# Euclidean ditance with average linkage clustering
-res2 <- hclust(euclidean, method = "average")
-
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "sbet_euclidean_average.pdf"))
-plot(res2, main = "Euclidean distance of standardized betas with average linkage clustering")
-dev.off()
-
-# Euclidean distance with single linkage clustering
-res3 <- hclust(euclidean, method = "single")
-
-# Plot cluster dendrogram
-pdf(paste0(BASE, "dendrograms/", "sbet_euclidean_single.pdf"))
-plot(res3, main = "Euclidean distance of standardized betas with average linkage clustering")
-dev.off()
+# plot
+plot_dendrogram(output = ALL_GENES, object = res1, head_title = c("Dendrogram of all effect sizes;"))
